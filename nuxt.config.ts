ca18c860af02f858f8f6f955fa489513452547e6
Afi Amouzou <afi.amouzou@burda-forward.de>
2026-05-19 14:44:06 +0200

    #1: Add tailwincss module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import tailwindcss from "@tailwindcss/vite"
-
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  typescript: {
+    // customize tsconfig.app.json
+    tsConfig: {
+     "compilerOptions": {
+        "strict": true,
+        "noImplicitAny": true,
+        "strictNullChecks": true,
+        "noUncheckedIndexedAccess": true,
+        "moduleResolution": "bundler",
+        "types": [
+         "node"
+        ]
+      }
+    }
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ],
 })
