0f94b3434ba3613219e21af7fcfe030be18f2fd5
Afi Amouzou <afi.amouzou@burda-forward.de>
2026-05-20 23:44:09 +0200

    #3: Merge eslint feature branch

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-   <h1 class="text-3xl font-bold underline">
-      Grace Church Frontend
-    </h1>
-  </div>
+    <div>
+        <NuxtRouteAnnouncer />
+        <h1 class="text-3xl font-bold underline">Grace Church Frontend</h1>
+    </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  typescript: {
-    // customize tsconfig.app.json
-    tsConfig: {
-     "compilerOptions": {
-        "strict": true,
-        "noImplicitAny": true,
-        "strictNullChecks": true,
-        "noUncheckedIndexedAccess": true,
-        "moduleResolution": "bundler",
-        "types": [
-         "node"
-        ]
-      }
-    }
-  },
-  modules: [
-    '@nuxtjs/tailwindcss',
-  ],
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
+    css: ['~/assets/css/main.css'],
+    typescript: {
+        // customize tsconfig.app.json
+        tsConfig: {
+            compilerOptions: {
+                strict: true,
+                noImplicitAny: true,
+                strictNullChecks: true,
+                noUncheckedIndexedAccess: true,
+                moduleResolution: 'bundler',
+                types: ['node'],
+            },
+        },
+    },
+    modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
 })
